306651c952fee5ac6c2cd084c6536704d66ece36
Marlena Dulęba <marlena_duleba@o2.pl>
2022-12-18 10:04:18 +0100

    task-01 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+let timer = null;
+
+startBtn.addEventListener('click', () => {
+  timer = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  stopBtn.disabled = false;
+  startBtn.disabled = true;
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timer);
+  stopBtn.disabled = true;
+  startBtn.disabled = false;
+});
