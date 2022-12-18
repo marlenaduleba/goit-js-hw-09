f1294322833c2c25d73243d2edfcf064b495459a
Marlena Dulęba <marlena_duleba@o2.pl>
2022-12-18 11:51:26 +0100

    task-02 flatpickr and notiflix added

---
+++ src/js/02-timer.js
@@ -0,0 +1,16 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import { Notify } from "notiflix/build/notiflix-notify-aio";
+import "notiflix/dist/notiflix-3.2.5.min.css";
+
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+      console.log(selectedDates[0]);
+    },
+  };
+
+  const fp = flatpickr("#datetime-picker", options);
