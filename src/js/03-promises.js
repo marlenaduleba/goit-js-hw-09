7d3a1dc7e13d23650495a381ae561a371117fe9e
Marlena Dulęba <marlena_duleba@o2.pl>
2022-12-26 19:45:28 +0100

    task-03 basic functions added

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,45 @@
+import Notiflix, { Notify } from 'notiflix';
+
+const form = document.querySelector(".form");
+const { delay, step, amount } = form;
+
+let totalDelay = 0;
+
+form.addEventListener("submit", event => {
+  event.preventDefault();
+  showPromises();
+});
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
+return new Promise((resolve, reject) => {
+  setTimeout(() => {
+    const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+    resolve({ position, delay });
   } else {
-    // Reject
+    reject({ position, delay });
   }
+  }, delay.value);
+  
+});
+  
 }
+
+function showPromises() {
+  
+    for (let i = 1; i <= amount.value; i++) {
+  let parsedDelay = Number.parseInt(delay.value);
+  let parsedStep = Number.parseInt(step.value);
+  totalDelay = parsedDelay + parsedStep * i;
+    createPromise(i, totalDelay)
+  .then(({ position, delay }) => {
+    Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+    
+}
+
+}
+
