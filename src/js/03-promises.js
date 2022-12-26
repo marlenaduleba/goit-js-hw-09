a640ab3dbbe508b045c24ce643501e21e082c543
Marlena Dulęba <marlena_duleba@o2.pl>
2022-12-26 20:49:43 +0100

    task-03 delay fixed

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,45 +1,39 @@
 import Notiflix, { Notify } from 'notiflix';
 
-const form = document.querySelector(".form");
+const form = document.querySelector('.form');
 const { delay, step, amount } = form;
 
 let totalDelay = 0;
 
-form.addEventListener("submit", event => {
+form.addEventListener('submit', event => {
   event.preventDefault();
   showPromises();
 });
 
 function createPromise(position, delay) {
-return new Promise((resolve, reject) => {
-  setTimeout(() => {
-    const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    resolve({ position, delay });
-  } else {
-    reject({ position, delay });
-  }
-  }, delay.value);
-  
-});
-  
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
 
 function showPromises() {
-  
-    for (let i = 1; i <= amount.value; i++) {
-  let parsedDelay = Number.parseInt(delay.value);
-  let parsedStep = Number.parseInt(step.value);
-  totalDelay = parsedDelay + parsedStep * i;
-    createPromise(i, totalDelay)
-  .then(({ position, delay }) => {
-    Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-  });
-    
+  for (let i = 0; i < amount.value; i++) {
+    let delayNumber = Number(delay.value);
+    let stepNumber = Number(step.value);
+    totalDelay = delayNumber + stepNumber * i;
+    createPromise(i + 1, totalDelay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
 }
-
-}
-
